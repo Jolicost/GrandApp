6139128d717696828532d2112490f08c7a6cb4df
Raul Montoya <raulomontoyaperez@gmail.com>
2018-11-06 20:23:08 +0100

    Login refactor + DataUtils

--- app/src/main/java/com/jauxim/grandapp/ui/Activity/ActivityLogin/ActivityLogin.java
+++ app/src/main/java/com/jauxim/grandapp/ui/Activity/ActivityLogin/ActivityLogin.java
@@ -2,12 +2,14 @@ package com.jauxim.grandapp.ui.Activity.ActivityLogin;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.jauxim.grandapp.R;
+import com.jauxim.grandapp.Utils.DataUtils;
 import com.jauxim.grandapp.Utils.Dialog;
 import com.jauxim.grandapp.networking.Service;
 import com.jauxim.grandapp.ui.Activity.BaseActivity;
@@ -16,10 +18,12 @@ import com.jauxim.grandapp.ui.Activity.Main.Main;
 import javax.inject.Inject;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 import static android.widget.Toast.LENGTH_SHORT;
 
-public class ActivityLogin extends BaseActivity implements View.OnClickListener, ActivityLoginView {
+public class ActivityLogin extends BaseActivity implements ActivityLoginView {
 
     @Inject
     public Service service;
@@ -39,21 +43,19 @@ public class ActivityLogin extends BaseActivity implements View.OnClickListener,
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        username = findViewById(R.id.username);
-        password = findViewById(R.id.password);
-        loginbutton = findViewById(R.id.loginbutton);
-        loginbutton.setOnClickListener(this);
-
+        ButterKnife.bind(this);
+        getDeps().inject(this);
         presenter = new ActivityLoginPresenter(service, this);
     }
 
-    @Override
-    public void onClick(View v) {
-        showWait();
-        String user = username.getText().toString();
-        String pass = password.getText().toString();
-        presenter.login(user,pass);
+    @OnClick(R.id.loginbutton)
+    public void loginClick(){
+        if (username != null && password != null) {
+            String user = username.getText().toString();
+            String pass = password.getText().toString();
+            showWait();
+            presenter.login(user, pass);
+        }
     }
 
     @Override
