db3d5150eb51ea5976a41230a9ef9e3e42a2922b
xapaal <xavi.p@monqi.co>
2018-12-28 15:56:36 +0100

    fixing mine

--- app/src/main/java/com/jauxim/grandapp/Constants.java
+++ app/src/main/java/com/jauxim/grandapp/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
 
     public static String ACTIVITY_ID = "activity_id";
     public static String PROFILE_ID = "profile_id";
-    public static String ACTIVITY_MINE = "min";
+    public static String ACTIVITY_MINE = "mine";
     public static String ACTIVITY_ALL = "all";
 }
