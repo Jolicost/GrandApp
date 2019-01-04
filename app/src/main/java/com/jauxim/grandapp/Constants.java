a0a9608d5f59531ec4bdf357faa972cdac95c898
xapaal <xavi.p@monqi.co>
2019-01-04 19:08:42 +0100

    adding guide

--- app/src/main/java/com/jauxim/grandapp/Constants.java
+++ app/src/main/java/com/jauxim/grandapp/Constants.java
@@ -8,4 +8,9 @@ public class Constants {
     public static String ACTIVITY_ALL = "all";
     public static int ACTIVITIES_PAGE = 7;
 
+
+    public static final String INTRO_FOCUS_MENU = "intro_focus_1";
+    public static final String INTRO_FOCUS_NEW_ACT = "intro_focus_2";
+    public static final String INTRO_FOCUS_FILTER_ACT = "intro_focus_3";
+
 }
